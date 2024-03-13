3b5b6bcefb612047b3c5627e6e60a636fc4a8806
DimaM83 <mdv16.01.83@gmail.com>
2024-03-13 08:56:44 +0100

    fix tak-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,15 @@
 const input = document.getElementById('name-input');
 const output = document.getElementById('name-output');
+const helloEl = document.querySelector('h1');
 
 input.addEventListener('input', () => {
-    const name = input.value.trim();
-    if (name === '') {
-        output.textContent = 'Anonymous';
-    } else {
-        output.textContent = name;
-    }
+  const name = input.value.trim();
+
+  if (name === '') {
+    helloEl.firstChild.textContent = '';
+    output.textContent = 'Anonymous';
+  } else {
+    helloEl.firstChild.textContent = 'Hello,';
+    output.textContent = name;
+  }
 });
